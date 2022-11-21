2b9f5dceb48e2f86b878bd36fd0bdbbbf92b3a0e
letsgot <itschauhan97@gmail.com>
2022-11-21 16:13:47 +0530

    added upto backend to deploy

---
+++ api.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+// npm i cookie parser
+const cookieParser = require("cookie-parser");
+// jsonwebtoken
+// token name is -> JWT & mechanism -> cookies
+// repersent -> collection
+const userRouter = require("./routes/userRoutes");
+const authRouter = require("./routes/authRoutes");
+const planRouter = require("./routes/planRoutes");
+
+const reviewModel = require("./model/reviewModel");
+const planModel = require("./model/planModel");
+
+
+// to  add post body data to req.body
+app.use(express.json());
+// add cookies to req.cookies
+app.use(cookieParser());
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/plan", planRouter);
+
+// update user Profile
+// delete user profile
+
+// create a review
+app.post("/api/v1/review",async (req,res) =>{
+    try {
+        let reviewData = req.body;
+        console.log(reviewData);
+        let review = await reviewModel.create(reviewData);
+        let rating = review.rating;
+        let reviewId = review["_id"];
+        let currentPlan = await planModel.findById(review.plan);
+        // average rating 
+        let totalNoofRating = currentPlan.reviews.length;
+        let prevAvg = currentPlan.averageRating;
+        if (prevAvg) {
+            let totalRatings = prevAvg * totalNoofRating;
+            let newAvg = (totalRatings + rating) / 
+            (totalNoofRating + 1);
+            currentPlan.averageRating = newAvg;
+        } else {
+            currentPlan.averageRating = rating;
+        }
+        currentPlan.reviews.push(reviewId);
+        await currentPlan.save();
+        res.status(201).json({
+            review,
+            result: "created"
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
+})
+
+app.get("/api/v1/allReviews", async (req,res) => {
+    try{
+       let reviews = await reviewModel.find()
+       .populate("user plan");
+
+       res.status(200).json({
+         reviews,
+         result : "All result send"
+       })
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({
+            message : err.message
+        })
+    }
+})
+
+// locahost:3000 -> express API 
+app.listen(3000, function () {
+    console.log("server started at port 3000");
+})
+
+
+
+
+
+// create -> deleteUser, updateUser
+// {
+//     name: 'Jasbir',
+//     password: 'abcd',
+//     confirmPassword: 'abcd',
+//     email: 'abc@gmail.com',
+//     phonenumber: '8800953687',
+//     pic: 'dp.png',
+// -> unnique id
+//     _id: new ObjectId("62d2f2c0aaa6d2fe55d1e68c"),
+// mongoose
+//     __v: 0
+//   }
