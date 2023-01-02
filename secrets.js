9b886143ac42eddac140ae54e28d270ca1f9c394
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-01-02 14:11:20 +0530

    Delete secrets.js

--- secrets.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    JWTSECRET : "chshfgdhshfgf",
-    APP_PASSWORD : "fsnyftagydwjckvx",
-    APP_EMAIL : "exception153@gmail.com",
-    DB_LINK : "mongodb+srv://itschauhan:CqaixCE50nVysyMh@cluster0.d48whtg.mongodb.net/?retryWrites=true&w=majority"
-}
